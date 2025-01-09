559c56ddcb841710be51b6d83e6b7c7d16bc9577
Himanshu Gangwar <151658698+zenitsu0509@users.noreply.github.com>
2025-01-09 19:28:58 +0530

    Update main.py

--- Linear_Regression/main.py
+++ Linear_Regression/main.py
@@ -3,7 +3,7 @@ data = pd.read_csv("/content/project_9_sample_dataset.csv")
 height = "Height"
 weight = "Weight"
 train,target = data[[height]],data[weight]
-data
+dat
 from sklearn.linear_model import LinearRegression
 linear_regression = LinearRegression()
 linear_regression.fit(train,target)
